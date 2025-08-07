098664fa0f6402e7b7e011e0e903521a1688a1ac
“Riaz <riazahmed3919@gmail.com>
2025-08-08 00:22:11 +0600

    Complete Changes for Module-19

---
+++ order/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from users.models import CustomUser
+from product.models import Product
+
+# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart of {self.user.first_name} {self.user.last_name}"
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+    
+class Order(models.Model):
+    PENDING = 'Pending'
+    SHIPPED = 'Shipped'
+    DELIVERED = 'Delivered'
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (SHIPPED, 'Shipped'),
+        (DELIVERED, 'Delivered')
+    ]
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
+    total_price = models.DecimalField(max_digits=9, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order id: {self.id} by {self.user.first_name} {self.user.last_name}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
